27014835507f52fe67c9fc9693606c976aceb083
SeonghakHong <tjdgkr0719@khu.ac.kr>
2025-11-07 22:06:08 +0900

    change protocol version error to warning

--- src/mcp_server.cpp
+++ src/mcp_server.cpp
@@ -755,16 +755,16 @@ json server::handle_initialize(const request& req, const std::string& session_id
     LOG_INFO("Client requested protocol version: ", requested_version);
 
     if (requested_version != MCP_VERSION) {
-        LOG_ERROR("Unsupported protocol version: ", requested_version, ", server supports: ", MCP_VERSION);
-        return response::create_error(
-            req.id, 
-            error_code::invalid_params, 
-            "Unsupported protocol version",
-            {
-                {"supported", {MCP_VERSION}},
-                {"requested", params["protocolVersion"]}
-            }
-        ).to_json();
+        LOG_WARNING("Unsupported protocol version: ", requested_version, ", server supports: ", MCP_VERSION);
+        // return response::create_error(
+        //     req.id, 
+        //     error_code::invalid_params, 
+        //     "Unsupported protocol version",
+        //     {
+        //         {"supported", {MCP_VERSION}},
+        //         {"requested", params["protocolVersion"]}
+        //     }
+        // ).to_json();
     }
 
     // Extract client info
